7db606e83dbbbc7e74a0794c33bea543f324a50e
Guido van Rossum <guido@python.org>
2020-07-16 21:32:04 -0700

    Use match/case in first_sets

--- Tools/peg_generator/pegen/first_sets.py
+++ Tools/peg_generator/pegen/first_sets.py
@@ -11,7 +11,6 @@ from pegen.grammar import (
     Cut,
     Gather,
     Grammar,
-    GrammarVisitor,
     Group,
     Leaf,
     Lookahead,
@@ -34,7 +33,7 @@ argparser = argparse.ArgumentParser(
 argparser.add_argument("grammar_file", help="The grammar file")
 
 
-class FirstSetCalculator(GrammarVisitor):
+class FirstSetCalculator:
     def __init__(self, rules: Dict[str, Rule]) -> None:
         self.rules = rules
         for rule in rules.values():
@@ -47,92 +46,84 @@ class FirstSetCalculator(GrammarVisitor):
             self.visit(rule)
         return self.first_sets
 
-    def visit_Alt(self, item: Alt) -> Set[str]:
-        result: Set[str] = set()
-        to_remove: Set[str] = set()
-        for other in item.items:
-            new_terminals = self.visit(other)
-            if isinstance(other.item, NegativeLookahead):
-                to_remove |= new_terminals
-            result |= new_terminals
-            if to_remove:
-                result -= to_remove
+    def visit(self, item) -> Set[str]:
+        match item:
+            case Alt():
+                result: Set[str] = set()
+                to_remove: Set[str] = set()
+                for other in item.items:
+                    new_terminals = self.visit(other)
+                    if isinstance(other.item, NegativeLookahead):
+                        to_remove |= new_terminals
+                    result |= new_terminals
+                    if to_remove:
+                        result -= to_remove
 
-            # If the set of new terminals can start with the empty string,
-            # it means that the item is completelly nullable and we should
-            # also considering at least the next item in case the current
-            # one fails to parse.
+                    # If the set of new terminals can start with the empty string,
+                    # it means that the item is completelly nullable and we should
+                    # also considering at least the next item in case the current
+                    # one fails to parse.
 
-            if "" in new_terminals:
-                continue
+                    if "" in new_terminals:
+                        continue
 
-            if not isinstance(other.item, (Opt, NegativeLookahead, Repeat0)):
-                break
+                    if not isinstance(other.item, (Opt, NegativeLookahead, Repeat0)):
+                        break
 
-        # Do not allow the empty string to propagate.
-        result.discard("")
+                # Do not allow the empty string to propagate.
+                result.discard("")
 
-        return result
+                return result
 
-    def visit_Cut(self, item: Cut) -> Set[str]:
-        return set()
+            case Cut():
+                return set()
 
-    def visit_Group(self, item: Group) -> Set[str]:
-        return self.visit(item.rhs)
+            case Group():
+                return self.visit(item.rhs)
 
-    def visit_PositiveLookahead(self, item: Lookahead) -> Set[str]:
-        return self.visit(item.node)
+            case NamedItem():
+                return self.visit(item.item)
 
-    def visit_NegativeLookahead(self, item: NegativeLookahead) -> Set[str]:
-        return self.visit(item.node)
+            case Lookahead() | Opt() | Gather() | Repeat():
+                return self.visit(item.node)
 
-    def visit_NamedItem(self, item: NamedItem) -> Set[str]:
-        return self.visit(item.item)
+            case NameLeaf(value=v) if v not in self.rules:
+                return {item.value}
 
-    def visit_Opt(self, item: Opt) -> Set[str]:
-        return self.visit(item.node)
+            case NameLeaf(value=v) if v not in self.first_sets:
+                self.first_sets[item.value] = self.visit(self.rules[item.value])
+                return self.first_sets[item.value]
 
-    def visit_Gather(self, item: Gather) -> Set[str]:
-        return self.visit(item.node)
+            case NameLeaf(value=v) if v in self.in_process:
+                return set()
 
-    def visit_Repeat0(self, item: Repeat0) -> Set[str]:
-        return self.visit(item.node)
+            case NameLeaf(value=v):
+                return self.first_sets[v]
 
-    def visit_Repeat1(self, item: Repeat1) -> Set[str]:
-        return self.visit(item.node)
+            case StringLeaf():
+                return {item.value}
 
-    def visit_NameLeaf(self, item: NameLeaf) -> Set[str]:
-        if item.value not in self.rules:
-            return {item.value}
+            case Rhs():
+                result: Set[str] = set()
+                for alt in item.alts:
+                    result |= self.visit(alt)
+                return result
 
-        if item.value not in self.first_sets:
-            self.first_sets[item.value] = self.visit(self.rules[item.value])
-            return self.first_sets[item.value]
-        elif item.value in self.in_process:
-            return set()
+            case Rule(name=name) if name in self.in_process:
+                return set()
 
-        return self.first_sets[item.value]
+            case Rule(name=name, rhs=rhs, nullable=nullable):
+                if name not in self.first_sets:
+                    self.in_process.add(name)
+                    terminals = self.visit(rhs)
+                    if nullable:
+                        terminals.add("")
+                    self.first_sets[name] = terminals
+                    self.in_process.remove(name)
+                return self.first_sets[name]
 
-    def visit_StringLeaf(self, item: StringLeaf) -> Set[str]:
-        return {item.value}
-
-    def visit_Rhs(self, item: Rhs) -> Set[str]:
-        result: Set[str] = set()
-        for alt in item.alts:
-            result |= self.visit(alt)
-        return result
-
-    def visit_Rule(self, item: Rule) -> Set[str]:
-        if item.name in self.in_process:
-            return set()
-        elif item.name not in self.first_sets:
-            self.in_process.add(item.name)
-            terminals = self.visit(item.rhs)
-            if item.nullable:
-                terminals.add("")
-            self.first_sets[item.name] = terminals
-            self.in_process.remove(item.name)
-        return self.first_sets[item.name]
+            case _:
+                assert False, item
 
 
 def main() -> None:
