e0a63c26c9ed91d49ca70cefa632a3be7d734d1f
Brandt Bucher <brandtbucher@microsoft.com>
2022-08-25 15:34:12 -0700

    Move uops into their own header

--- Python/ceval.c
+++ Python/ceval.c
@@ -33,6 +33,7 @@
 #include "pydtrace.h"
 #include "setobject.h"
 #include "structmember.h"         // struct PyMemberDef, T_OFFSET_EX
+#include "pycore_uops.h"
 
 #include <ctype.h>
 #include <stdbool.h>
@@ -989,120 +990,6 @@ typedef struct {
 
 #define KWNAMES_LEN() \
     (call_shape.kwnames == NULL ? 0 : ((int)PyTuple_GET_SIZE(call_shape.kwnames)))
-
-// uops go here! ///////////////////////////////////////////////////////////////
-
-#define UOP_CHECK_EVAL_BREAKER() _uop_check_eval_breaker()
-#define UOP_CHECK_TRACING()      _uop_check_tracing(cframe, &opcode)
-#define UOP_DISPATCH()           _uop_dispatch()
-#define UOP_EXTEND_OPARG()       _uop_extend_oparg(&oparg, next_instr)
-#define UOP_INCREF()             _uop_incref(stack_pointer)
-#define UOP_JUMP(I)              _uop_jump(&next_instr, (I))
-#define UOP_LLTRACE()            _uop_lltrace()
-#define UOP_LOAD_CONST(O, I)     _uop_load_const(&(O), consts, (I))
-#define UOP_NEXT_OPARG()         _uop_next_oparg(next_instr, &oparg)
-#define UOP_NEXT_OPCODE()        _uop_next_opcode(next_instr, &opcode)
-#define UOP_STACK_GROW(I)        _uop_stack_grow(&stack_pointer, (I))
-#define UOP_STACK_SET(I, O)      _uop_stack_set(stack_pointer, (I), (O))
-#define UOP_UNREACHABLE()        _uop_unreachable()
-#define UOP_UPDATE_STATS()       _uop_update_stats()
-#define UOP_WARMUP()             _uop_warmup(frame)
-#define UOP_WRITE_PREV_INSTR()   _uop_write_prev_instr(frame, next_instr)
-
-#define _uop_check_eval_breaker() CHECK_EVAL_BREAKER()
-
-static inline Py_ALWAYS_INLINE void
-_uop_check_tracing(_PyCFrame cframe, uint8_t *opcode_p)
-{
-    assert(cframe.use_tracing == 0 || cframe.use_tracing == 255);
-    *opcode_p |= cframe.use_tracing OR_DTRACE_LINE;
-}
-
-#define _uop_dispatch() DISPATCH_GOTO()
-
-static inline Py_ALWAYS_INLINE void
-_uop_extend_oparg(int *oparg_p, _Py_CODEUNIT *next_instr)
-{
-    *oparg_p <<= 8;
-    *oparg_p |= _Py_OPARG(*next_instr);
-}
-
-static inline Py_ALWAYS_INLINE void
-_uop_incref(PyObject **stack_pointer)
-{
-    Py_INCREF(stack_pointer[-1]);
-}
-
-static inline Py_ALWAYS_INLINE void
-_uop_jump(_Py_CODEUNIT **next_instr_p, int i)
-{
-    *next_instr_p += i;
-}
-
-#define _uop_lltrace() PRE_DISPATCH_GOTO()
-
-static inline Py_ALWAYS_INLINE void
-_uop_load_const(PyObject **o_p, PyObject *consts, int i)
-{
-    *o_p = GETITEM(consts, i);
-}
-
-static inline Py_ALWAYS_INLINE void
-_uop_next_oparg(_Py_CODEUNIT *next_instr, int *oparg_p)
-{
-    *oparg_p = _Py_OPARG(*next_instr);
-}
-
-static inline Py_ALWAYS_INLINE void
-_uop_next_opcode(_Py_CODEUNIT *next_instr, uint8_t *opcode_p)
-{
-    *opcode_p = _Py_OPCODE(*next_instr);
-}
-
-static inline Py_ALWAYS_INLINE void
-_uop_stack_grow(PyObject ***stack_pointer_p, int i)
-{
-    *stack_pointer_p += i;
-}
-
-static inline Py_ALWAYS_INLINE void
-_uop_stack_set(PyObject **stack_pointer, int i, PyObject *o)
-{
-    stack_pointer[-i] = o;
-}
-
-static inline Py_ALWAYS_INLINE void
-_uop_unreachable()
-{
-    Py_UNREACHABLE();
-}
-
-#ifdef Py_STATS
-    #define _uop_update_stats()                                       \
-        do {                                                          \
-            OPCODE_EXE_INC(op);                                       \
-            if (_py_stats) {                                          \
-                _py_stats->opcode_stats[lastopcode].pair_count[op]++; \
-            }                                                         \
-            lastopcode = op;                                          \
-        } while (0)
-#else
-    #define _uop_update_stats()
-#endif
-
-static inline Py_ALWAYS_INLINE void
-_uop_warmup(_PyInterpreterFrame *frame)
-{
-    _PyCode_Warmup(frame->f_code);
-}
-
-static inline Py_ALWAYS_INLINE void
-_uop_write_prev_instr(_PyInterpreterFrame *frame, _Py_CODEUNIT *next_instr)
-{
-    frame->prev_instr = next_instr;
-}
-
-////////////////////////////////////////////////////////////////////////////////
 
 PyObject* _Py_HOT_FUNCTION
 _PyEval_EvalFrameDefault(PyThreadState *tstate, _PyInterpreterFrame *frame, int throwflag)
