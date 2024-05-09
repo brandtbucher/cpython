bc99eded392ffc1b1d4380d95fd1527f40694c3c
Brandt Bucher <brandtbucher@microsoft.com>
2024-05-08 18:09:19 -0700

    Remove guards on for loop iterators

--- Python/optimizer_cases.c.h
+++ Python/optimizer_cases.c.h
@@ -1388,6 +1388,14 @@
         /* _INSTRUMENTED_FOR_ITER is not a viable micro-op for tier 2 */
 
         case _ITER_CHECK_LIST: {
+            _Py_UopsSymbol *iter;
+            iter = stack_pointer[-1];
+            if (sym_matches_type(iter, &PyListIter_Type)) {
+                REPLACE_OP(this_instr, _NOP, 0, 0);
+            }
+            if (!sym_set_type(iter, &PyListIter_Type)) {
+                goto hit_bottom;
+            }
             break;
         }
 
@@ -1407,6 +1415,14 @@
         }
 
         case _ITER_CHECK_TUPLE: {
+            _Py_UopsSymbol *iter;
+            iter = stack_pointer[-1];
+            if (sym_matches_type(iter, &PyTupleIter_Type)) {
+                REPLACE_OP(this_instr, _NOP, 0, 0);
+            }
+            if (!sym_set_type(iter, &PyTupleIter_Type)) {
+                goto hit_bottom;
+            }
             break;
         }
 
@@ -1426,6 +1442,14 @@
         }
 
         case _ITER_CHECK_RANGE: {
+            _Py_UopsSymbol *iter;
+            iter = stack_pointer[-1];
+            if (sym_matches_type(iter, &PyRangeIter_Type)) {
+                REPLACE_OP(this_instr, _NOP, 0, 0);
+            }
+            if (!sym_set_type(iter, &PyRangeIter_Type)) {
+                goto hit_bottom;
+            }
             break;
         }
 
