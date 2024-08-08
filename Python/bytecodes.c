1c271064ed51d234cffe7dbcd83f8a46ded824df
Brandt Bucher <brandtbucher@microsoft.com>
2024-08-08 11:07:01 -0700

    Start every instruction sequence with a "backward jump"

--- Python/bytecodes.c
+++ Python/bytecodes.c
@@ -4685,7 +4685,7 @@ dummy_func(
 #endif
             uintptr_t eval_breaker = _Py_atomic_load_uintptr_relaxed(&tstate->eval_breaker);
             DEOPT_IF(eval_breaker & _PY_EVAL_EVENTS_MASK);
-            assert(tstate->tracing || eval_breaker == FT_ATOMIC_LOAD_UINTPTR_ACQUIRE(_PyFrame_GetCode(frame)->_co_instrumentation_version));
+            // assert(tstate->tracing || eval_breaker == FT_ATOMIC_LOAD_UINTPTR_ACQUIRE(_PyFrame_GetCode(frame)->_co_instrumentation_version));
         }
 
 // END BYTECODES //
